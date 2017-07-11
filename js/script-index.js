84fa4f4c501f2791013aec28a0e195371e63ba75
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-07-10 23:48:52 -0400

    Añadiendo , etapa4 lista, 5 y 6 listo

--- js/script-index.js
+++ js/script-index.js
@@ -27,16 +27,88 @@ function printNews(e){
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
-	for (var i= 0;i<recipesArray.length; i++){
-		if (highlighted == true) {
-			return(renderRecipe(recipesArray));
-			console.log('Recipes: ', recipesArray);
-		} else {
-		console.log("no se cumple la condicion");
-	}
+	console.log('Recipes: ', recipesArray);
+	//for (var i= 0;i<recipesArray.length; i++){
+		//if (highlighted == true) {
+			//return(renderRecipe(recipesArray));
+	    //ETAPA 4 y 5: solo titulos y autores
+	    for(var e in recipe){ /*es el de recorrer un objeto pasando por cada una de sus 
+	    	propiedades para actuar sobre ellas de alguna manera*/
+    	var titulo = recipe["title"];
+    	console.log(recipe["title"]);
+    	var sou = recipe["source"].name;
+    	console.log(recipe["source"].name);
+    	var name = recipe["name"];
+    
+    
+		var link= $('<a/>', {
+			'class': 'item-recipe'});
+		var spancito= $('<span/>',{
+			'class': 'attribution'});
+		var spantitle= $('<span/>',{
+			'class': 'title-recipe'});
+		var span1= $('<span/>',{
+			'class': 'metadata-recipe'});
+		var span2= $('<span/>',{
+			'class': 'author-recipe'});
+		var span3= $('<span/>',{
+			'class': 'bookmarks-recipe'});
+    	var span4= $('<span/>',{
+    		'class': 'icon-bookmark'});
+    	var imagen = $('<img/>',{
+    		'src':'img/recipes/320x350/' + name + '.jpg'});
+
+    span3.append(span4);
+    span1.append(span3);
+    span1.append(span2);
+    span2.append(sou);
+    spantitle.append(titulo);
+    spancito.append(spantitle);
+    spancito.append(span1);
+    link.append(spancito);
+    link.append(link);   
+   $('.list-recipes').append(link);
+    renderActivities(activitiesArray);
 }
-renderHighlightedRecipes();
+
+if(activitiesArray.length > 0){
+		$(".wrapper-message").hide();
+
+			
+			//$(".list-recipes").append("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'>" +  e.title + "</span><span class='metadata-recipe'><span class='author-recipe'>" + e.source.name + "</span><span class='bookmarks-recipe'><span class='icon-bookmark'></span> </span></span></span><img src='img/recipes/320x350/"+ $(e).attr("name") +".jpg'></a>");
+	/*("<a/>", {
+    class: 'item-recipe'     // hijos.
+    href :''
+}).append($('<span/>',{
+    class: 'attribution'
+
+}).append("<span/>", {
+    class: 'title-recipe'
+    text: 'Titulo de la Receta'
+    attr: 'title'
+})).hide().appendTo('list-recipes').fadeIn('slow');*/
+	 //$("a").append("class='item-recipe' href='#'");
+			/*.append('<span class="attribution"</span>')
+			$("<span/>" , {
+					html : 'Un <strong>Titulo de la receta</strong> ',
+    				'class' : 'title-recipe',
+    				attr : 'title'
+			});
+			
+			append("<span class='metadata-recipe'></span>")
+			
+			$("<span/>" , {
+					html : '<strong>NOMBRE DEL AUTO DE LA RECETA</strong> ',
+    				'class' : 'author-recipe',
+    				attr : 'source.name'
+			});
+			append("<span class='bookmarks-recipe'>")
+			append("<span class='icon-bookmark'></span>")
+			append("<img src='assets/img/recipes/320x350/spring-fromage-fort.jpg'")*/
+
+
 }
+
 
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
@@ -44,18 +116,11 @@ renderHighlightedRecipes();
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
-var agregarInfo= $("<a></a>");
-		agregarInfo.addClass("item-recipe");
-		agregarInfo.attr("href", "#");
-		$("div.list-recipes").append(agregarInfo);
-		
 
 function renderRecipe(recipe) {
-	
-
 	console.log('Voy a pintar la receta: ', recipe);
 }
-
+renderRecipe();
 
 
 /*
